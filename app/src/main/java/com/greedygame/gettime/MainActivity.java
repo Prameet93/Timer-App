01125177e12700d8b1e03515e9182ad45b6352e6
Prameet31 <prameet31o@gmail.com>
2017-03-24 11:36:49 +0530

    Second commit

--- app/src/main/java/com/greedygame/gettime/MainActivity.java
+++ app/src/main/java/com/greedygame/gettime/MainActivity.java
@@ -15,20 +15,7 @@ import android.widget.TextView;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+1:00"));
-        Date currentLocalTime = cal.getTime();
-        DateFormat date = new SimpleDateFormat("HH:mm a");
-        date.setTimeZone(TimeZone.getTimeZone("GMT+5:30"));
-        String localTime = date.format(currentLocalTime);
-        SharedPreferences prefs = this.getSharedPreferences(
-                "com.greedygame.gettime", Context.MODE_PRIVATE);
-        String dateTimeKey = "com.greedygame.gettime";
-        prefs.edit().putString(dateTimeKey,localTime.toString()).commit();
-    }
+
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView textView = (TextView) findViewById(R.id.textView);
         TextView currentTime = (TextView) findViewById(R.id.textView1);
+        TextView closeTime = (TextView) findViewById(R.id.textView3);
         TextView newTime = (TextView) findViewById(R.id.textView2);
         Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+1:00"));
         Date currentLocalTime = cal.getTime();
@@ -50,8 +38,20 @@ public class MainActivity extends AppCompatActivity {
         String l = prefs.getString(dateTimeKey, "no data exists");
 
         newTime.setText(l);
-
-
+    }
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+1:00"));
+        Date currentLocalTime = cal.getTime();
+        DateFormat date = new SimpleDateFormat("HH:mm a");
+        date.setTimeZone(TimeZone.getTimeZone("GMT+5:30"));
+        String localTime = date.format(currentLocalTime);
+        SharedPreferences prefs = this.getSharedPreferences(
+                "com.greedygame.gettime", Context.MODE_PRIVATE);
+        String dateTimeKey = "com.greedygame.gettime";
+        prefs.edit().putString(dateTimeKey,localTime.toString()).commit();
     }
 }
 
